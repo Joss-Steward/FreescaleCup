a12da53dc27c6f0622e2dc0c875184eda9acb6bd
Joss.Steward <joss.steward@gmail.com>
2015-09-21 11:51:19 -0400

    Changing some indentation

--- FRDM-TFC/Sources/AlgoOne.c
+++ FRDM-TFC/Sources/AlgoOne.c
@@ -12,73 +12,73 @@
 #define STOP_PIXEL 118
 
 void delay(int x){
-	int i;
-	for(i = 0; i < 100000*x; i++);
+   int i;
+   for(i = 0; i < 100000*x; i++);
 }
 
 void run(){
-	
-	int i;
-	
-	//Variables for calculating average light levels
-	long int sum = 0;
-	int avg;
-	
-	//Array recording if pixel light level is below average
-	int belowAvg[ STOP_PIXEL - START_PIXEL + 1 ]; 
-	
-	//Variables for calculating center of the pixels below average and difference from center of all pixels
-	int sumBelowAvg = 0;
-	int totBelowAvg = 0;
-	int centerBelowAvg;
-	int center = ( STOP_PIXEL - START_PIXEL + 1) / 2;
-	int diffCenter;
-	float diff;
-	
-	
-	if( LineScanImageReady == 1 ){
-		LineScanImageReady = 0;
-		
-		//Adds each pixels light value to the total
-		for( i = START_PIXEL; i < STOP_PIXEL; i++ ){
-			sum += LineScanImage0[i];
-		}
-		
-		//Calculates average light value
-		avg = sum / ( STOP_PIXEL - START_PIXEL );
-		
-		//Calculates which pixels are below average and fills the array accordingly
-		for( i = START_PIXEL; i < STOP_PIXEL; i++ ){
-			
-			//If pixel is below average sets corresponding array element to 1
-			if( LineScanImage0[i] < avg ){
-				belowAvg[ i - START_PIXEL ] = 1;
-			}
-			
-			//If pixel is above average sets corresponding array element to 0 
-			else {
-				belowAvg[ i - START_PIXEL ] = 0;
-			}
-		}
-		
-		//Outputs the array over serial
-		for( i = START_PIXEL; i < STOP_PIXEL; i++ ){
-			printf( "%d", belowAvg[ i - START_PIXEL ] );
-		}
-		printf( "\r\n" );
-	}
-	
-	//Adds all the pixels below average
-	for( i = 0; i < STOP_PIXEL - START_PIXEL + 1; i++){
-		if( belowAvg[i] ){
-			totBelowAvg++;
-			sumBelowAvg += i;
-		}
-	}
-	
-	centerBelowAvg = sumBelowAvg / totBelowAvg; //calculates the center of the below average pixels
-	diffCenter = center - centerBelowAvg;  //Clalculates the difference of the center of the below average pixels and all the pixels
-	diff = (float)( center - diffCenter ) / (float)center; //Calculates "percent" difference. Ranges from 0 - 2
-	TFC_SetServo(0,diff - 1); //Adjusts servo accordingly
-	delay(2); //Allows servo time to move
+
+   int i;
+
+   //Variables for calculating average light levels
+   long int sum = 0;
+   int avg;
+
+   //Array recording if pixel light level is below average
+   int belowAvg[ STOP_PIXEL - START_PIXEL + 1 ];
+
+   //Variables for calculating center of the pixels below average and difference from center of all pixels
+   int sumBelowAvg = 0;
+   int totBelowAvg = 0;
+   int centerBelowAvg;
+   int center = ( STOP_PIXEL - START_PIXEL + 1) / 2;
+   int diffCenter;
+   float diff;
+
+
+   if( LineScanImageReady == 1 ){
+      LineScanImageReady = 0;
+
+      //Adds each pixels light value to the total
+      for( i = START_PIXEL; i < STOP_PIXEL; i++ ){
+         sum += LineScanImage0[i];
+      }
+
+      //Calculates average light value
+      avg = sum / ( STOP_PIXEL - START_PIXEL );
+
+      //Calculates which pixels are below average and fills the array accordingly
+      for( i = START_PIXEL; i < STOP_PIXEL; i++ ){
+
+         //If pixel is below average sets corresponding array element to 1
+         if( LineScanImage0[i] < avg ){
+            belowAvg[ i - START_PIXEL ] = 1;
+         }
+
+         //If pixel is above average sets corresponding array element to 0
+         else {
+            belowAvg[ i - START_PIXEL ] = 0;
+         }
+      }
+
+      //Outputs the array over serial
+      for( i = START_PIXEL; i < STOP_PIXEL; i++ ){
+         printf( "%d", belowAvg[ i - START_PIXEL ] );
+      }
+      printf( "\r\n" );
+   }
+
+   //Adds all the pixels below average
+   for( i = 0; i < STOP_PIXEL - START_PIXEL + 1; i++){
+      if( belowAvg[i] ){
+         totBelowAvg++;
+         sumBelowAvg += i;
+      }
+   }
+
+   centerBelowAvg = sumBelowAvg / totBelowAvg; //calculates the center of the below average pixels
+   diffCenter = center - centerBelowAvg;  //Clalculates the difference of the center of the below average pixels and all the pixels
+   diff = (float)( center - diffCenter ) / (float)center; //Calculates "percent" difference. Ranges from 0 - 2
+   TFC_SetServo(0,diff - 1); //Adjusts servo accordingly
+   delay(2); //Allows servo time to move
 }
